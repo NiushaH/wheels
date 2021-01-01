cbabe2883f088d50c4dc1fbb6b3b45d55abc923f
Niusha H <35556570+NiushaH@users.noreply.github.com>
2020-12-31 17:23:01 -0700

    Update App to Ruby 2.7, update gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_063706) do
+ActiveRecord::Schema.define(version: 2021_01_01_002041) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "year"
